68648d7ee1c7941461208a6cce94eb438c6b6f6d
jian5753 <41563423+jian5753@users.noreply.github.com>
2018-12-07 12:05:21 +0800

    ???

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect,render_to_response,get_object_or_404
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth import login,logout
@@ -12,7 +12,6 @@ def signup(request):
         if form.is_valid():
             username=request.POST['username']
             form.save()
-<<<<<<< HEAD
             #create new attendance record for new user
             courseLst=date_course.objects.all()
             user=User.objects.get(username=username)
@@ -20,34 +19,25 @@ def signup(request):
                 attendanceSheet.objects.create(user=user,course=course)
             
             #return HttpResponse(user)
-            return redirect('/collaAdmin/teacherpage') #remeber the slash at the very front
-=======
-            return redirect('/accounts/login/') #remeber the slash at the very front
->>>>>>> 0c6ac2f65327aef5a76fafe16794ffbe361ddf07
+            return redirect('/collaAdmin/teacherpage ') #remeber the slash at the very front
     else:
         form=UserCreationForm()
     return render(request,'accounts/signup.html',locals())
 
 def login_views(request,timecode):
-    if request.method == 'POST':
+    if request.method=='POST':
         form=AuthenticationForm(data=request.POST)
         if form.is_valid():
             #login the user
             user=form.get_user()
             login(request,user)
-            if user is not None:
-                if request.user.is_superuser:
-                    return redirect('/collaAdmin/teacherpage')
-                else:
-                    if timecode==0:
-                        return redirect('/collaAdmin/userProfile/')
-                    else: 
-                        return redirect('sign',time = timecode)
+            if request.user.is_authenticated:
+                return redirect('/collaAdmin/teacherpage')
+            else:
+                return HttpResponse("student page here")
     else:
-        form = AuthenticationForm()
-    
-    return render(request,'accounts/login.html',locals())
-
+        form=AuthenticationForm
+    return render(request,'accounts/login1.html',locals())
 
 def logout_views(request):
     logout(request)
